9bc7dc744fbc8f78b1e9e97bddc66c40411f3317
chr.m <chri.marios@gmail.com>
2023-06-21 08:47:31 +0300

    Create a Basic Program, Part 2 - State Management

---
+++ src/module_3/basic_program/student_intro_part2/src/instruction.rs
@@ -0,0 +1,30 @@
+use borsh::BorshDeserialize;
+use solana_program::program_error::ProgramError;
+
+pub enum IntroInstruction {
+    CreateAccount { name: String, message: String },
+}
+
+#[derive(BorshDeserialize)]
+pub struct StudentIntro {
+    name: String,
+    message: String,
+}
+
+impl IntroInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+
+        let payload = StudentIntro::try_from_slice(rest).unwrap();
+
+        Ok(match variant {
+            0 => IntroInstruction::CreateAccount {
+                name: payload.name,
+                message: payload.message,
+            },
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
