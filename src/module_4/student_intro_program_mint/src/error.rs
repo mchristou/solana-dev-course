adbc7a7db15cdb14eda99f14e98460bed9482de4
chr.m <chri.marios@gmail.com>
2023-06-26 22:18:46 +0300

    Cross Program Invocations

---
+++ src/module_4/student_intro_program_mint/src/error.rs
@@ -0,0 +1,23 @@
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum StudentIntroError {
+    #[error("Account not initialized yet")]
+    UninitializedAccount,
+
+    #[error("PDA derived does not equal PDA passed in")]
+    InvalidPDA,
+
+    #[error("Input data exceeds max length")]
+    InvalidDataLength,
+
+    #[error("IncorrectAccountError")]
+    IncorrectAccount,
+}
+
+impl From<StudentIntroError> for ProgramError {
+    fn from(e: StudentIntroError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
---
+++ src/module_4/student_intro_program_mint/src/processor.rs
@@ -0,0 +1,407 @@
+use crate::error::StudentIntroError;
+use crate::instruction::IntroInstruction;
+use crate::state::{Reply, ReplyCounter, StudentInfo};
+use borsh::BorshSerialize;
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    borsh::try_from_slice_unchecked,
+    entrypoint::ProgramResult,
+    msg,
+    native_token::LAMPORTS_PER_SOL,
+    program::invoke_signed,
+    program_error::ProgramError,
+    program_pack::IsInitialized,
+    pubkey::Pubkey,
+    system_instruction,
+    sysvar::{rent::Rent, Sysvar},
+};
+use spl_associated_token_account::get_associated_token_address;
+use spl_token::{instruction::initialize_mint, ID as TOKEN_PROGRAM_ID};
+use std::convert::TryInto;
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = IntroInstruction::unpack(instruction_data)?;
+    match instruction {
+        IntroInstruction::CreateAccount {
+            name,
+            message,
+        } => add_student_intro(program_id, accounts, name, message),
+        IntroInstruction::UpdateAccount {
+            name,
+            message,
+        } => update_student_intro(program_id, accounts, name, message),
+        IntroInstruction::Reply {
+            reply,
+        } => add_reply(program_id, accounts, reply),
+        IntroInstruction::InitializeMint => {
+            initialize_token_mint(program_id, accounts)
+        }
+    }
+}
+
+pub fn initialize_token_mint(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let initializer = next_account_info(account_info_iter)?;
+    let token_mint = next_account_info(account_info_iter)?;
+    let mint_auth = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+    let token_program = next_account_info(account_info_iter)?;
+    let sysvar_rent = next_account_info(account_info_iter)?;
+
+    let (mint_pda, mint_bump) =
+        Pubkey::find_program_address(&[b"token_mint"], program_id);
+    let (mint_auth_pda, _mint_auth_bump) =
+        Pubkey::find_program_address(&[b"token_auth"], program_id);
+
+    msg!("Token mint: {:?}", mint_pda);
+    msg!("Mint authority: {:?}", mint_auth_pda);
+
+    if mint_pda != *token_mint.key {
+        msg!("Incorrect token mint account");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    if *token_program.key != TOKEN_PROGRAM_ID {
+        msg!("Incorrect token program");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    if *mint_auth.key != mint_auth_pda {
+        msg!("Incorrect mint auth account");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(82);
+
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            token_mint.key,
+            rent_lamports,
+            82,
+            token_program.key,
+        ),
+        &[initializer.clone(), token_mint.clone(), system_program.clone()],
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Created token mint account");
+
+    invoke_signed(
+        &initialize_mint(
+            token_program.key,
+            token_mint.key,
+            mint_auth.key,
+            Option::None,
+            9,
+        )?,
+        &[token_mint.clone(), sysvar_rent.clone(), mint_auth.clone()],
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Initialized token mint");
+
+    Ok(())
+}
+
+pub fn add_student_intro(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    name: String,
+    message: String,
+) -> ProgramResult {
+    msg!("Adding student intro...");
+    msg!("Name: {}", name);
+    msg!("Message: {}", message);
+
+    let account_info_iter = &mut accounts.iter();
+
+    let initializer = next_account_info(account_info_iter)?;
+    let pda_account = next_account_info(account_info_iter)?;
+    let pda_counter = next_account_info(account_info_iter)?;
+    let token_mint = next_account_info(account_info_iter)?;
+    let mint_auth = next_account_info(account_info_iter)?;
+    let user_ata = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+    let token_program = next_account_info(account_info_iter)?;
+    let reply_counter = next_account_info(account_info_iter)?;
+    let user_account = next_account_info(account_info_iter)?;
+
+    let (mint_pda, mint_bump) =
+        Pubkey::find_program_address(&[b"token_mint"], program_id);
+    let (mint_auth_pda, _mint_auth_bump) =
+        Pubkey::find_program_address(&[b"token_auth"], program_id);
+
+    if *token_mint.key != mint_pda {
+        msg!("Incorrect token mint");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    if *mint_auth.key != mint_auth_pda {
+        msg!("Mint passed in and mint derived do not match");
+        return Err(StudentIntroError::InvalidPDA.into());
+    }
+
+    if *user_ata.key
+        != get_associated_token_address(initializer.key, token_mint.key)
+    {
+        msg!("Incorrect token mint");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    if *token_program.key != TOKEN_PROGRAM_ID {
+        msg!("Incorrect token program");
+        return Err(StudentIntroError::IncorrectAccount.into());
+    }
+
+    let (pda, bump_seed) =
+        Pubkey::find_program_address(&[initializer.key.as_ref()], program_id);
+
+    if pda != *user_account.key {
+        msg!("Invalid seeds for PDA");
+        return Err(StudentIntroError::InvalidPDA.into());
+    }
+
+    msg!("Minting 10 tokens to User associated token account");
+    invoke_signed(
+        // Instruction
+        &spl_token::instruction::mint_to(
+            token_program.key,
+            token_mint.key,
+            user_ata.key,
+            mint_auth.key,
+            &[],
+            10 * LAMPORTS_PER_SOL,
+        )?,
+        // Account_infos
+        &[token_mint.clone(), user_ata.clone(), mint_auth.clone()],
+        // Seeds
+        &[&[b"token_mint", &[mint_bump]]],
+    )?;
+
+    msg!("Tokens minted");
+
+    let studentinfo_discriminator = "studentinfo";
+    let total_len: usize = (4 + studentinfo_discriminator.len())
+        + 1
+        + (4 + name.len())
+        + (4 + message.len());
+    if total_len > 1000 {
+        msg!("Data length is larger than 1000 bytes");
+        return Err(StudentIntroError::InvalidDataLength.into());
+    }
+    let account_len: usize = 1000;
+
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(account_len);
+
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            user_account.key,
+            rent_lamports,
+            account_len.try_into().unwrap(),
+            program_id,
+        ),
+        &[initializer.clone(), user_account.clone(), system_program.clone()],
+        &[&[initializer.key.as_ref(), &[bump_seed]]],
+    )?;
+
+    msg!("PDA created: {}", pda);
+
+    let mut account_data =
+        try_from_slice_unchecked::<StudentInfo>(&user_account.data.borrow())
+            .unwrap();
+
+    if account_data.is_initialized() {
+        msg!("Account already initialized");
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    account_data.name = name;
+    account_data.msg = message;
+    account_data.is_initialized = true;
+    account_data.serialize(&mut &mut user_account.data.borrow_mut()[..])?;
+
+    let counter_discriminator = "counter";
+    let counter_len: usize = (4 + counter_discriminator.len()) + 1 + 1;
+
+    let rent = Rent::get()?;
+    let counter_rent_lamports = rent.minimum_balance(counter_len);
+
+    let (counter, counter_bump) = Pubkey::find_program_address(
+        &[pda.as_ref(), "reply".as_ref()],
+        program_id,
+    );
+    if counter != *reply_counter.key {
+        msg!("Invalid seeds for PDA");
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            reply_counter.key,
+            counter_rent_lamports,
+            counter_len.try_into().unwrap(),
+            program_id,
+        ),
+        &[initializer.clone(), reply_counter.clone(), system_program.clone()],
+        &[&[pda.as_ref(), "reply".as_ref(), &[counter_bump]]],
+    )?;
+    msg!("reply counter created");
+
+    let mut counter_data =
+        try_from_slice_unchecked::<ReplyCounter>(&reply_counter.data.borrow())
+            .unwrap();
+
+    if counter_data.is_initialized() {
+        msg!("Account already initialized");
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    counter_data.discriminator = counter_discriminator.to_string();
+    counter_data.counter = 0;
+    counter_data.is_initialized = true;
+    msg!("reply count: {}", counter_data.counter);
+    counter_data.serialize(&mut &mut reply_counter.data.borrow_mut()[..])?;
+
+    Ok(())
+}
+
+pub fn update_student_intro(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    name: String,
+    message: String,
+) -> ProgramResult {
+    msg!("Updating student intro...");
+    msg!("Name: {}", name);
+    msg!("Message: {}", message);
+    let account_info_iter = &mut accounts.iter();
+
+    let initializer = next_account_info(account_info_iter)?;
+    let user_account = next_account_info(account_info_iter)?;
+
+    let mut account_data =
+        try_from_slice_unchecked::<StudentInfo>(&user_account.data.borrow())
+            .unwrap();
+
+    if !account_data.is_initialized() {
+        msg!("Account is not initialized");
+        return Err(StudentIntroError::UninitializedAccount.into());
+    }
+
+    if user_account.owner != program_id {
+        return Err(ProgramError::IllegalOwner);
+    }
+
+    let (pda, _bump_seed) =
+        Pubkey::find_program_address(&[initializer.key.as_ref()], program_id);
+
+    if pda != *user_account.key {
+        msg!("Invalid seeds for PDA");
+        return Err(StudentIntroError::InvalidPDA.into());
+    }
+
+    let update_len: usize =
+        1 + (4 + account_data.name.len()) + (4 + message.len());
+
+    if update_len > 1000 {
+        msg!("Data length is larger than 1000 bytes");
+        return Err(StudentIntroError::InvalidDataLength.into());
+    }
+
+    account_data.msg = message;
+    account_data.serialize(&mut &mut user_account.data.borrow_mut()[..])?;
+
+    Ok(())
+}
+
+pub fn add_reply(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    reply: String,
+) -> ProgramResult {
+    msg!("Adding Reply...");
+    msg!("Reply: {}", reply);
+
+    let account_info_iter = &mut accounts.iter();
+
+    let replier = next_account_info(account_info_iter)?;
+    let user_account = next_account_info(account_info_iter)?;
+    let reply_counter = next_account_info(account_info_iter)?;
+    let reply_account = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    let mut counter_data =
+        try_from_slice_unchecked::<ReplyCounter>(&reply_counter.data.borrow())
+            .unwrap();
+
+    let reply_discriminator = "reply";
+    let account_len: usize =
+        (4 + reply_discriminator.len()) + 1 + 32 + (4 + reply.len());
+
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(account_len);
+
+    let (pda, bump_seed) = Pubkey::find_program_address(
+        &[
+            user_account.key.as_ref(),
+            counter_data.counter.to_be_bytes().as_ref(),
+        ],
+        program_id,
+    );
+
+    if pda != *reply_account.key {
+        msg!("Invalid seeds for PDA");
+        return Err(StudentIntroError::InvalidPDA.into());
+    }
+
+    invoke_signed(
+        &system_instruction::create_account(
+            replier.key,
+            reply_account.key,
+            rent_lamports,
+            account_len.try_into().unwrap(),
+            program_id,
+        ),
+        &[replier.clone(), reply_account.clone(), system_program.clone()],
+        &[&[
+            user_account.key.as_ref(),
+            counter_data.counter.to_be_bytes().as_ref(),
+            &[bump_seed],
+        ]],
+    )?;
+
+    let mut reply_data =
+        try_from_slice_unchecked::<Reply>(&reply_account.data.borrow())
+            .unwrap();
+
+    if reply_data.is_initialized() {
+        msg!("Account already initialized");
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    reply_data.discriminator = reply_discriminator.to_string();
+    reply_data.studentinfo = *user_account.key;
+    reply_data.reply = reply;
+    reply_data.is_initialized = true;
+    reply_data.serialize(&mut &mut reply_account.data.borrow_mut()[..])?;
+
+    msg!("Reply Count: {}", counter_data.counter);
+
+    counter_data.counter += 1;
+    counter_data.serialize(&mut &mut reply_counter.data.borrow_mut()[..])?;
+
+    Ok(())
+}
